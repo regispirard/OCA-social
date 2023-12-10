784f7e6dc6d8397342e332dd8444c287bdefd682
regispirard <regis.pirard@tincid.com>
2023-12-10 17:48:56 +0100

    [UPD]update

--- mail_activity_team_auto_assign/models/mail_activity.py
+++ mail_activity_team_auto_assign/models/mail_activity.py
@@ -1,8 +1,12 @@
 # Copyright 2018-22 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from odoo import api, models
 from odoo.tools.safe_eval import safe_eval
+
+_logger = logging.getLogger(__name__)
 
 
 class MailActivity(models.Model):
@@ -22,13 +26,17 @@ class MailActivity(models.Model):
             team_rules_domain, order="sequence ASC"
         )
         if team_rules:
+            # record to which the activity is related
             record = self.env[self.res_model].search([("id", "=", self.res_id)])
-            for rule in team_rules:
-                # test if the rule applies to the record related to the activity
-                # the first rule which applies will be selected
-                if record.filtered_domain(safe_eval(rule.rule_domain)):
-                    # if the rule applies, assign to the user of the rule
-                    self.user_id = rule.team_member_id
-                    return
-        # Fall back to original activity_tem on_change_team_id if no rule applies
+            try:
+                for rule in team_rules:
+                    # the first rule which applies to the record will be selected
+                    if record.filtered_domain(safe_eval(rule.rule_domain)):
+                        # if the rule applies, assign activity to the user
+                        self.user_id = rule.team_member_id
+                        return
+            except KeyError:
+                # this exception can happen if the domain on the rule is not well defined
+                _logger.warning("Error with rule domain %s", rule.rule_domain)
+        # Fall back to original activity_team on_change_team_id if no rule applies
         return super()._onchange_team_id()
--- mail_activity_team_auto_assign/models/mail_activity_team_rule.py
+++ mail_activity_team_auto_assign/models/mail_activity_team_rule.py
@@ -16,16 +16,38 @@ class MailActivityTeamRule(models.Model):
         required="True",
         string="Team",
     )
+    team_member_ids = fields.Many2many(
+        related="team_id.member_ids",
+        readonly=True,
+    )
+    team_model_ids = fields.Many2many(
+        related="team_id.res_model_ids",
+        readonly=True,
+    )
     team_member_id = fields.Many2one(
         comodel_name="res.users",
         required="True",
         string="Team Member",
+        domain="[('id','in',team_member_ids)]",
+    )
+    activity_model_ids = fields.Many2many(
+        comodel_name="ir.model",
+        compute="_compute_activity_model_ids",
     )
     rule_model = fields.Many2one(
         comodel_name="ir.model",
         string="Model",
+        domain="[('id','in',team_model_ids or activity_model_ids)]",
     )
     rule_domain = fields.Char(
         required="True",
         string="Rule",
     )
+
+    def _compute_activity_model_ids(self):
+        for rule in self:
+            rule.activity_model_ids = (
+                self.env["ir.model"]
+                .sudo()
+                .search(["&", ("is_mail_thread", "=", True), ("transient", "=", False)])
+            )
